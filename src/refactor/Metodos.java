1f28fe6e0820969e089734faa99ab30614d449f4
klemavilanova <klemavilanova@victoria18.danielcastelao.org>
2016-12-01 09:58:03 +0100

    refactCalcPrio

--- src/refactor/Metodos.java
+++ src/refactor/Metodos.java
@@ -38,32 +38,38 @@ public class Metodos {
         return ndigitos;
     }
 
+    public int calcLimite(int i) {
+        int limite = (i - 1) / 2;
+        if (limite % 2 == 0) {
+            limite--;
+        }
+        return limite;
+    }
+
+    public void calcPrimo(int i, int limite) {
+        int contador1 = 0;
+        int i1 = 1;
+        while (i1 <= limite) {
+            if (i % i1 == 0) {
+                contador1++;
+            }
+            i1 += 2;
+            if (contador1 == 2) {
+                i1 = limite + 1;
+            }
+        }
+        if (contador1 == 1) {
+            p = true;
+        }
+    }
+
     public void esPrimo(int i) {
         if (i < 4) {
             p = true;
         } else if (i % 2 == 0) {
             p = false;
         } else {
-            int contador1 = 0;
-            int i1 = 1;
-            int limite = (i - 1) / 2;
-
-            if (limite % 2 == 0) {
-                limite--;
-            }
-
-            while (i1 <= limite) {
-                if (i % i1 == 0) {
-                    contador1++;
-                }
-                i1 += 2;
-                if (contador1 == 2) {
-                    i1 = limite + 1;
-                }
-            }
-            if (contador1 == 1) {
-                p = true;
-            }
+            this.calcPrimo(i, this.calcLimite(i));
         }
         if (p == true) {
             System.out.println(i);
